df945a148b1b482719acf6b990fedfc2dd11f80d
unknown <shaikhaaqib12@yahoo.com>
2021-03-23 23:55:19 +0000

    First commit

---
+++ src/main/java/com/akber/demo/Controller.java
@@ -0,0 +1,13 @@
+package com.akber.demo;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @RequestMapping("/health")
+    public String returnStatus(){
+        return "InService";
+    }
+}
